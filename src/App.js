c680a60be17a6aab294da02b416ad53f46a35bd2
pisces330 <pisces2978@gmail.com>
2023-09-14 13:39:33 +0900

    react-router-dom install 및 App.js 수정

--- src/App.js
+++ src/App.js
@@ -1,5 +1,13 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Main } from "./components";
 const App = () => {
-  return <div>App</div>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route exact path="/" element={<Main />} />
+      </Routes>
+    </BrowserRouter>
+  );
 };
 
 export default App;
